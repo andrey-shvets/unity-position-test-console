f735bad77bf9c54a59f405d230825937825ecb8f
Andrey Shvets <sas@vava.cars>
2023-03-02 15:02:45 +0100

    Init rock-paper-scissors project

---
+++ RockPaperScissorsGame/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("[The music from the movie Saw is playing.]");
+Console.WriteLine("You don't know me, but I know you. I want to play a game. Here's what happens if you lose.");
+Console.WriteLine("First of all, you're going to be a bit disappointed with the whole situation. And that's about it.");
